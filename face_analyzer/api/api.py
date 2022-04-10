7f695cd6a9a3219210216c325eadc55e191c36a9
Benoist Wolleb <benoist.wolleb@la-cave.ch>
2022-04-10 13:08:28 +0200

    Send result as callback

--- face_analyzer/api/api.py
+++ face_analyzer/api/api.py
@@ -1,53 +1,43 @@
+import os
 import asyncio
-from fastapi import FastAPI, File, UploadFile
+import httpx
+from fastapi import FastAPI, File, UploadFile, Depends
 
 from .worker import Worker, Callback
 from . import interface
-import json
+import pydantic
 
 async def startup():
+	# Announce ourself to the engine
+	if engine is not None and service is not None:
+		serviceDescr = {"url": service + "/compute", "api": interface.engineAPI()}
+		await client.post(engine + "/services", json=serviceDescr)
+
 	worker.chain(callback)
 	callback.start()
 	worker.start()
 
 async def shutdown():
+	# Remove ourself from the engine
+	if engine is not None and service is not None:
+		endpoint = interface.engineAPI()["route"]
+		await client.delete(engine + "/services/" + endpoint)
+
 	await worker.stop()
 	await callback.stop()
 
-stackResult = []
-worker = Worker(stackResult)
-callback = Callback(stackResult)
+engine = os.environ["APP_ENGINE"] if "APP_ENGINE" in os.environ else None
+service = os.environ["APP_SERVICE"] if "APP_SERVICE" in os.environ else None
+
+client = httpx.AsyncClient()
+worker = Worker()
+callback = Callback()
 app = FastAPI(on_startup=[startup], on_shutdown=[shutdown])
 
-# Implement me!!! Define meaningful routes here, using input objects from "interface" or UploadFile + query params
-
-# # This is a route for a service that only takes json input, which structure should be a interface.Job object
-# @app.post("/compute", response_model = interface.TaskId)
-# async def post(data: interface.Job, callback_url: str = None, task_id: str = None):
-# 	if task_id is None:
-# 		task_id = str(interface.uid())
-# 	task = {"callback_url": callback_url, "task_id": task_id, "data": data.dict()}
-# 	await worker.addTask(task)
-# 	return interface.TaskId(task_id=task_id)
-
-# This is a route for a service that takes a binary file as input, plus a custom "data1" query param
 @app.post("/compute", response_model = interface.TaskId)
 async def post(img: UploadFile, callback_url: str = None, task_id: str = None):
 	if task_id is None:
 		task_id = str(interface.uid())
 	task = {"callback_url": callback_url, "task_id": task_id, "img": img}
-	print("[API] process")
 	await worker.addTask(task)
 	return interface.TaskId(task_id=task_id)
-
-@app.post("/result", response_model = interface.TaskId)
-async def post(callback_url: str = None, task_id: str = None):
-	if task_id is None:
-		task_id = str(interface.uid())
-	t = interface.TaskId(task_id=task_id)
-	print(type(t), t)
-	if(not stackResult):
-		return t
-	el = stackResult.pop(0)
-	t = interface.TaskId(task_id=json.dumps(el))	
-	return t
--- face_analyzer/api/worker.py
+++ face_analyzer/api/worker.py
@@ -1,17 +1,16 @@
 import asyncio
 import httpx
 import io
-from PIL import Image
 from deepface import DeepFace
+from PIL import Image
 import numpy as np
 
 class Worker():
-	def __init__(self, stackResult):
+	def __init__(self):
 		self.asyncTask = None
 		self.running = False
 		self.queue = asyncio.Queue()
 		self.next = None
-		self.stackResult = stackResult
 
 	def start(self):
 		self.running = True
@@ -37,35 +36,22 @@ class Worker():
 					await self.next.addTask(result)
 
 	async def process(self, task):
-		# Implement me with something useful!!!
-		print("[Worker] Processing...")
 		raw = await task["img"].read()
 		buff = io.BytesIO(raw)
 		img_pil = Image.open(buff)
 		img = np.array(img_pil)
-
 		diagnos = DeepFace.analyze(img_path=img , actions=['age', 'gender', 'race', 'emotion'])
-		res = {}
-		res["age"] = diagnos["age"]
-		res["region"] = diagnos["region"]
-		res["race"] = diagnos["dominant_race"]
-		res["emotion"] = diagnos["dominant_emotion"]
-		task["result"] = { "answer": res }
-		
-		self.stackResult.append(task["result"])
-		print("[Worker] Proscess : \n", task["result"])
+		task["result"] = diagnos
+
 		return task
 
 class Callback(Worker):
-	def __init__(self, stackResult):
-		super().__init__(stackResult)
+	def __init__(self):
+		super().__init__()
 		self.client = httpx.AsyncClient()
 
 	async def process(self, task):
-		print("[Callback] Process")
 		url = task["callback_url"]
 		task_id = task["task_id"]
 		if url is not None:
-			# Needs to be modified if the result contains a blob to send back
-			# something like: await self.client.post(url, params={"task_id": task_id}, files={"result": file...})
 			await self.client.post(url, params={"task_id": task_id}, json=task["result"])
