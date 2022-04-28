0e6f4fcc8e11ac587dd3fcb1f144a5d71d9d4e81
Benoist Wolleb <benoist.wolleb@la-cave.ch>
2022-04-28 20:26:17 +0200

    Announce face-detection service to the engine

--- face_detection/main.py
+++ face_detection/main.py
@@ -7,14 +7,8 @@ from api import interface
 def main():
 	host = os.environ["APP_HOST"] if "APP_HOST" in os.environ else "0.0.0.0"
 	port = int(os.environ["APP_PORT"]) if "APP_PORT" in os.environ else 8080
-	engine = os.environ["APP_ENGINE"] if "APP_ENGINE" in os.environ else None
 	log = os.environ["APP_LOG"] if "APP_LOG" in os.environ else "info"
 
-	if engine is not None:
-		try:
-			requests.post(engine, json=interface.engineAPI())
-		except:
-			print("Failed to notify engine")
 	uvicorn.run("api.api:app", host=host, port=port, log_level=log)
 
 if __name__ == "__main__":
