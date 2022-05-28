715d6105f1aad0159c10a91b214a617cdef84c0f
Nicolas Paschoud <nicolas.paschoud@master.hes-so.ch>
2022-05-28 16:27:46 +0000

    feat : Use dagre to visualize a pipeline correctly

--- webapp/src/board/Board.js
+++ webapp/src/board/Board.js
@@ -1,47 +1,47 @@
-import React, { useEffect } from 'react';
-import ReactFlow, { Background, Controls, useNodesState, useEdgesState } from 'react-flow-renderer';
+import React, { useEffect, useCallback } from 'react';
+import ReactFlow, { addEdge, ReactFlowProvider, Background, Controls, useNodesState, useEdgesState, MiniMap } from 'react-flow-renderer';
 
-import DrawGraph from './DrawGraph'
+import DrawGraph from './DrawGraph';
 
 
-function Board({ service }) {
-  const [nodes, setNodes, onNodesChange] = useNodesState([{
-    id: 1,
-    next: [],
-    sourcePosition: "right",
-    targetPosition: 'left',
-    data: { label: "Nothing to se here currently, please select a service to see its content" },
-    position: { x: 0, y: 0 }
-  }]);
+const Board = ({ service }) => {
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge({ ...params, animated: true }, eds)),
+    []
+  );
+
+
   useEffect(() => {
-    if (Object.keys(service).length > 0){
-      const newGraph = DrawGraph(service);
-      setNodes(newGraph[0]);
-      setEdges(newGraph[1]);
-    }
+    const { nodes: layoutedNodes, edges: layoutedEdges } = DrawGraph(service);
+    setNodes([...layoutedNodes]);
+    setEdges([...layoutedEdges]);
   }, [service])
 
   return (
-    <div className='column' style={{ height: "70vh" }}>
+    <div className="column" style={{ height: "70vh"}}>
       <div className="content">
         <h4>{Object.keys(service).length > 0 ? service.nodes[0].api.route : "No pipeline selected"}</h4>
         <p>{Object.keys(service).length > 0 ? service.nodes[0].api.summary : ""}</p>
       </div>
-      <ReactFlow
-        id="board"
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        fitView
-      >
-        <Controls />
-        <Background />
-      </ReactFlow>
+      <ReactFlowProvider>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          fitView
+        >
+          <Controls />
+          <Background />
+          <MiniMap/>
+        </ReactFlow>
+      </ReactFlowProvider>
     </div>
   );
-}
+};
 
 export default Board;
