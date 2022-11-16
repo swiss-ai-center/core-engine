45a9adbeb7ce366da56ad92cd99f67d3be09bd2f
Andrea Petrucci <andrea.petrucci@edu.hefr.ch>
2022-11-16 14:48:31 +0100

    removed unused routes in tasks

--- engine/src/tasks/controller.py
+++ engine/src/tasks/controller.py
@@ -7,7 +7,8 @@ from common.query_parameters.skip_and_limit import SkipAndLimit
 router = APIRouter()
 
 
-@router.get("/tasks/{task_id}", summary="Get results of a task", responses={404: {"description": "Task Not Found"}}, response_model=TaskSchema)
+@router.get("/tasks/{task_id}", summary="Get results of a task", responses={404: {"description": "Task Not Found"}},
+            response_model=TaskSchema)
 async def get_task_result(task_id: str, tasks_service: TasksService = Depends()):
     task = tasks_service.find_one(task_id)
 
@@ -17,26 +18,6 @@ async def get_task_result(task_id: str, tasks_service: TasksService = Depends())
     dto = TaskSchema.toTaskSchema(task)
 
     return dto
-
-
-@router.get("/tasks/{taskId}/status", summary="Get the status of a task")
-async def get_task_status(taskId: str):
-    # return await engine.pollTask(taskId)
-    return None
-
-
-@router.get("/tasks/{taskId}/raw", summary="Get raw pipeline data for a task")
-async def get_task_raw(taskId: str):
-    # raw = await engine.getJobRaw(taskId)$
-    raw = None
-    return JSONResponse(raw)
-
-
-@router.get("/tasks/{taskId}/files/{fileName}", summary="Retrieve a binary result of a task")
-async def get_task_result_file(taskId: str, fileName: str):
-    # stream = await engine.getResultFile(taskId, fileName)
-    stream = None
-    return StreamingResponse(stream, media_type="application/octet-stream")
 
 
 @router.get("/tasks", summary="Find many tasks", description="Find many tasks.", response_model=list[TaskSchema])
