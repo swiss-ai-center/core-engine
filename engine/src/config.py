5d02fc6a341ed97d02c4ff0f1fcae8dbcc8125a0
Andrea Petrucci <andrea.petrucci@edu.hefr.ch>
2022-11-16 15:15:56 +0100

    lint settings

--- engine/src/config.py
+++ engine/src/config.py
@@ -1,12 +1,14 @@
 from functools import lru_cache
 from pydantic import BaseSettings
 
+
 class Settings(BaseSettings):
     database_url: str = "sqlite:///../engine.db"
     database_connect_args: dict[str, bool | str | int] = {"check_same_thread": False}
 
     class Config:
         env_file = "../.env"
+
 
 @lru_cache()
 def get_settings():
