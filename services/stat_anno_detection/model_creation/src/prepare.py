12694605bb7a3afd61c8558d7138b2521a5305dd
Florian Feuillade <florian.feuillade@he-arc.ch>
2022-11-29 02:37:05 +0100

    Dvc is working in local

--- services/stat_anno_detection/model_creation/src/prepare.py
+++ services/stat_anno_detection/model_creation/src/prepare.py
@@ -1,10 +1,11 @@
 import pandas as pd
-
+from pathlib import Path
 
 def main():
     df = pd.read_csv("datasets/145_UCR_Anomaly_Lab2Cmac011215EPG1_5000_17210_17260.txt", names=["value"])
     df.index = pd.to_datetime(df.index)
-    df.to_pickle("datasets/145_UCR_Anomaly_Lab2Cmac011215EPG1_5000_17210_17260.pkl")
+    Path("datasets/prepare").mkdir(parents=True, exist_ok=True)
+    df.to_pickle("datasets/prepare/145_UCR_Anomaly_Lab2Cmac011215EPG1_5000_17210_17260.pkl", )
 
 if __name__ == '__main__':
     main()
