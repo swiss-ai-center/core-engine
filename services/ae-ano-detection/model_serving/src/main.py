91d3a4297af54e8e42439c3a8c3e57d909aa84da
Florian Feuillade <florian.feuillade@he-arc.ch>
2023-04-03 15:53:02 +0200

    Fix workflow

--- services/ae-ano-detection/model_serving/src/main.py
+++ services/ae-ano-detection/model_serving/src/main.py
@@ -49,7 +49,7 @@ class MyService(Service):
                 FieldDescription(name="result", type=[FieldDescriptionType.IMAGE_PNG]),
             ]
         )
-        self.model = tf.keras.models.load_model("../model/ae_model.h5")
+        self.model = tf.keras.models.load_model("../ae_model.h5")
 
     async def process(self, data):
         raw = str(data["text"].data)[2:-1]
