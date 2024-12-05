917c64d03afbc4ab832c254db54da9adabc7260f
Andrea Petrucci <andrea.petrucci@hefr.ch>
2024-12-05 17:13:24 +0100

    228 add alembic (#229)
    Co-authored-by: Charlie Junod <charlie.junod@hefr.ch>

---
+++ backend/alembic/versions/26299cf5a3b2_add_an_aoptional_error_message_field_to_.py
@@ -0,0 +1,23 @@
+"""add an aoptional 'error_message' field to Task
+
+Revision ID: 26299cf5a3b2
+Revises:
+Create Date: 2024-11-27 16:09:14.473532
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '26299cf5a3b2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('tasks', sa.Column('error_message', sa.String(), nullable=True))
+
+
+def downgrade():
+    op.drop_column("tasks", "error_message")
