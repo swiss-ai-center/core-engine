d3099f2ac163ac66204b4ee93ccdaccae9b8f7af
valentinbiolley <valentin.biolley@hefr.ch>
2025-10-10 11:23:55 +0200

    fix : Remove unused import 'inspect' from env.py
    - Align code with Flake8

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,7 +1,7 @@
 import os
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, inspect
+from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
